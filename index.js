8c8ddbae27427c3343212e1c54b83cbd6c859aa3
Mahesh Kumar <ml4909854@gmail.com>
2025-09-21 17:36:31 +0530

    backend

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.get("/health", (req, res) => {
   res.status(200).send({ status: "ok", message: "Server is running!" });
 });
 
-// 404 handler (for all other routes)
-app.use((req, res) => {
-  res.status(404).send({ status: "error", message: "Page not found!" });
-});
+
 
 
 // routes
